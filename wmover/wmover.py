f6a515417e5330403b98b40f6e971cf6ad2f63e7
Jacob-Vlijm <vlijm@planet.nl>
2017-08-19 10:51:22 +0200

    Fixing latest push from wrong local dir...

---
+++ wmover/wmover.py
@@ -0,0 +1,54 @@
+import gi.repository
+gi.require_version('Budgie', '1.0')
+from gi.repository import Budgie, GObject, Gtk, Gdk
+import subprocess
+    
+
+class WMover(GObject.GObject, Budgie.Plugin):
+    """ This is simply an entry point into your Budgie Applet implementation.
+        Note you must always override Object, and implement Plugin.
+    """
+
+    # Good manners, make sure we have unique name in GObject type system
+    __gtype_name__ = "WMover"
+
+    def __init__(self):
+        """ Initialisation is important.
+        """
+        GObject.Object.__init__(self)
+        
+    def do_get_panel_widget(self, uuid):
+        """ This is where the real fun happens. Return a new Budgie.Applet
+            instance with the given UUID. The UUID is determined by the
+            BudgiePanelManager, and is used for lifetime tracking.
+        """
+        return WMoverApplet(uuid)
+
+
+class WMoverApplet(Budgie.Applet):
+    """ Budgie.Applet is in fact a Gtk.Bin """
+    manager = None
+
+    def __init__(self, uuid):
+        Budgie.Applet.__init__(self)
+        self.box = Gtk.EventBox()
+        icon = Gtk.Image.new_from_file("/opt/wmover/misc/wmover22.png")
+        self.box.add(icon)        
+        self.add(self.box)
+        self.popover = Budgie.Popover.new(self.box)
+        self.hello = Gtk.Label("Window Mover is active")
+        self.popover.add(self.hello)        
+        self.popover.get_child().show_all()
+
+        self.box.show_all()
+        self.show_all()
+        self.box.connect("button-press-event", self.on_press)
+        subprocess.Popen("/opt/wmover/wmover/wmover_panelrunner")
+
+    def	on_press(self, box, arg):
+        self.manager.show_popover(self.box)
+
+    def do_update_popovers(self, manager):
+    	self.manager = manager
+    	self.manager.register_popover(self.box, self.popover)
+
