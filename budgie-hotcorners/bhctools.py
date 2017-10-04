1f9f9f8437a19d4e70481e20c9cbb534ecb12147
Jacob-Vlijm <vlijm@planet.nl>
2017-10-04 19:37:17 +0200

    Added license section.

--- budgie-hotcorners/bhctools.py
+++ budgie-hotcorners/bhctools.py
@@ -2,8 +2,22 @@
 import subprocess
 import os
 
+"""
+Budgie Hot Corners
+Author: Jacob Vlijm
+Copyright=Copyright © 2017 Ubuntu Budgie Developers
+Website=https://ubuntubudgie.org
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or any later version. This
+program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE. See the GNU General Public License for more details. You
+should have received a copy of the GNU General Public License along with this
+program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 # config path
-##dr = os.path.join(os.environ["HOME"], ".config/budgie-hotcorners")
 dr = os.path.join(os.environ["HOME"], ".config", "budgie-extras", "hotcorners")
 # settings file
 settings = os.path.join(dr, "hotc_settings")
