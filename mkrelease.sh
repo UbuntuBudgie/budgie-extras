88f6ee93be932d133e7f1068d05252bd9c4390c8
David Mohammed <fossfreedom@ubuntu.com>
2017-12-01 15:45:52 +0000

    Add release mechanism, signing with fossfreedoms debian key

---
+++ mkrelease.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+VERSION="0.3"
+NAME="budgie-extras"
+git-archive-all.sh --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
+xz -9 "${NAME}-${VERSION}.tar"
+
+gpg --default-key 1E1FB0017C998A8AE2C498A6C2EAA8A26ADC59EE --armor --detach-sign "${NAME}-${VERSION}.tar.xz"
+gpg --verify "${NAME}-${VERSION}.tar.xz.asc"               
