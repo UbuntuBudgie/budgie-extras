e00053cb06c7c06ee5ab39ca529719d1b57d2ccf
Jacob-Vlijm <vlijm@planet.nl>
2017-12-26 17:16:13 +0100

    Updated copyright year.

--- budgie-hotcorners/budgie_hcorners.py
+++ budgie-hotcorners/budgie_hcorners.py
@@ -12,7 +12,7 @@ from bhctools import get, dr, settings
 """
 Hot Corners
 Author: Jacob Vlijm
-Copyright=Copyright © 2017 Ubuntu Budgie Developers
+Copyright © 2017-2018 Ubuntu Budgie Developers
 Website=https://ubuntubudgie.org
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
