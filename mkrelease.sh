f75213b6eca320f2c0e3fadb2f90c078075e6719
David Mohammed <fossfreedom@ubuntu.com>
2018-01-15 23:24:18 +0000

    Version update to mkrelease

--- mkrelease.sh
+++ mkrelease.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION="0.3.4"
+VERSION="0.4.0"
 NAME="budgie-extras"
 git-archive-all.sh --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
 xz -9 "${NAME}-${VERSION}.tar"
