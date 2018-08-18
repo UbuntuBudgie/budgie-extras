f7f37acce2b47240b638d36b299ae3bb68b97bb3
David Mohammed <fossfreedom@ubuntu.com>
2018-08-18 10:49:44 +0100

    up the mkrelease version

--- mkrelease.sh
+++ mkrelease.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
-VERSION="0.5.0"
+VERSION="0.6.0"
 NAME="budgie-extras"
 git-archive-all.sh --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
 xz -9 "${NAME}-${VERSION}.tar"
 
 gpg --default-key 1E1FB0017C998A8AE2C498A6C2EAA8A26ADC59EE --armor --detach-sign "${NAME}-${VERSION}.tar.xz"
-gpg --verify "${NAME}-${VERSION}.tar.xz.asc"               
+gpg --verify "${NAME}-${VERSION}.tar.xz.asc"
+
