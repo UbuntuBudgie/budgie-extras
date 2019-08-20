25e343faf71ba6b9cbdb358ec310cdaee5c29f79
David Mohammed <fossfreedom@ubuntu.com>
2019-08-20 21:48:23 +0100

    Fix pep8 issues with subst.py

--- budgie-wpreviews/data/subst.py
+++ budgie-wpreviews/data/subst.py
@@ -6,15 +6,15 @@ import sys
 
 inputfile = sys.argv[1]
 replacetext = sys.argv[2]
-outputfile= sys.argv[3]
+outputfile = sys.argv[3]
 
 # Read in the file
-with open(inputfile, 'r') as file :
-  filedata = file.read()
+with open(inputfile, 'r') as file:
+    filedata = file.read()
 
 # Replace the target string
 filedata = filedata.replace('PATH_LOC', replacetext)
 
 # Write the file out again
 with open(outputfile, 'w') as file:
-  file.write(filedata)
+    file.write(filedata)
