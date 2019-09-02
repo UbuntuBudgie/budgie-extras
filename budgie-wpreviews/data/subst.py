08f1d4e846deaa7672162a2d284b026c13f75c9f
David Mohammed <fossfreedom@ubuntu.com>
2019-09-02 20:41:21 +0100

    Add previews menu translation control #213

--- budgie-wpreviews/data/subst.py
+++ budgie-wpreviews/data/subst.py
@@ -7,6 +7,9 @@ import sys
 inputfile = sys.argv[1]
 replacetext = sys.argv[2]
 outputfile = sys.argv[3]
+args = len(sys.argv)
+if args == 5:
+    podir = sys.argv[4]
 
 # Read in the file
 with open(inputfile, 'r') as file:
@@ -16,5 +19,16 @@ with open(inputfile, 'r') as file:
 filedata = filedata.replace('PATH_LOC', replacetext)
 
 # Write the file out again
-with open(outputfile, 'w') as file:
+if args == 5:
+    staging = "staging"
+else:
+    staging = ""
+with open(outputfile + staging, 'w') as file:
     file.write(filedata)
+
+if args == 5:
+    subprocess.run(['intltool-merge',
+        '--desktop-style',
+        podir,
+        outputfile + staging,
+        outputfile])
