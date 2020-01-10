690a80ddb5450930d3eeda66fb702fb575e8a9df
David Mohammed <fossfreedom@ubuntu.com>
2020-01-10 19:54:18 +0000

    v0.90.0

--- mkrelease.sh
+++ mkrelease.sh
@@ -4,7 +4,7 @@ set -e
 git submodule init
 git submodule update
 
-VERSION="0.10.1"
+VERSION="0.90.0"
 NAME="budgie-extras"
 git-archive-all.sh --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
 xz -9 "${NAME}-${VERSION}.tar"
