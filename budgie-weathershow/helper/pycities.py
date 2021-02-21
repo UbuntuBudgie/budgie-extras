4f1e05fec28e05fc45561f2378aa7a1ee2ae0c9e
David Mohammed <fossfreedom@ubuntu.com>
2021-02-21 00:16:48 +0000

    Fix pep8 issues

--- budgie-weathershow/helper/pycities.py
+++ budgie-weathershow/helper/pycities.py
@@ -1,27 +1,27 @@
 # helper app to convert openweather cities json dump to a flat file format
-# take the city.list.json (uncompress the file) from http://bulk.openweathermap.org/sample/
+# take the city.list.json (uncompress the file) from
+# http://bulk.openweathermap.org/sample/
 
 import json
 import math
 
-# really truncate floats rather than rounding
 
+# really truncate floats rather than rounding
 def truncate(number, digits) -> float:
     stepper = 10.0 ** digits
     return math.trunc(stepper * number) / stepper
 
-states={}
+
+states = {}
 
 # load the json file from openweather
-
 with open('city.list.json') as f:
-    data=json.load(f)
+    data = json.load(f)
 
 # load a json file with US states mapping to real names
 # save into a dict to be used for searching later
-
 with open('states.json') as s:
-    statesjson=json.load(s)
+    statesjson = json.load(s)
     for element in statesjson:
         states[element['abbreviation']] = element['name']
 
@@ -32,10 +32,10 @@ with open('cities', 'w') as f:
     for element in data:
 
         state = ""
-        country = element['country'] 
+        country = element['country']
         if country == "US":
-            if element['state'] == '00': #United States
-                continue # odd data in dump to ignore
+            if element['state'] == '00':  # United States
+                continue  # odd data in dump to ignore
 
             if element['state'] != '':
                 state = ", " + states[element['state']]
@@ -49,7 +49,7 @@ with open('cities', 'w') as f:
         # the city file potentially has lots of duplicates
         # filter these out - we are looking for
         # places that are very close geographically
-        
+
         if keycheck in duplicates:
             coord = duplicates[keycheck]
             lat = truncate(coord['lat'], 1)
@@ -59,9 +59,10 @@ with open('cities', 'w') as f:
             comparelon = truncate(element['coord']['lon'], 1)
 
             if lat == comparelat and lon == comparelon:
-                # the saved keycheck is the same in terms of lat, lon (1 decimal place)
+                # the saved keycheck is the same in terms of lat,
+                # lon (1 decimal place)
                 continue
         else:
             duplicates[keycheck] = element['coord']
-                
+
         f.write(line)
