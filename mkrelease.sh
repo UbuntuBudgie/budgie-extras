ac033a617d4c03a8c87b87ff882d111035c112fc
David Mohammed <fossfreedom@ubuntu.com>
2022-03-10 23:24:44 +0000

    Bump version for stable release

--- mkrelease.sh
+++ mkrelease.sh
@@ -4,7 +4,7 @@ set -e
 git submodule init
 git submodule update
 
-VERSION="1.3.91"
+VERSION="1.4.0"
 NAME="budgie-extras"
 git-archive-all.sh --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
 xz -9 "${NAME}-${VERSION}.tar"
