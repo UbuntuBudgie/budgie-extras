5ecbea4c60406cbc902bd2648cf8e1a2c34f005f
Jacob-Vlijm <vlijm@planet.nl>
2023-02-17 09:14:51 +0100

    Added script to extract names, added vala file with extracted names.

---
+++ budgie-hotcorners/misc/extract_names.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import sys
+import ast
+
+"""
+Use this script to extract the name field of hotcorner's defaults.in.in file
+into a entrynames.vala - file
+"""
+
+lines = open("./defaults.in.in", "r").readlines()
+target = "./entrynames.vala"
+
+with open(target, "wt") as valafile:
+    for l in lines:
+        dictline = ast.literal_eval(l)
+        name = dictline["name"]
+        valafile.write('_("%s")\n' % name)
+
+        
+        
+    
+
+
