ea6608db95656bfc197b95535386e901bb068a51
David Mohammed <fossfreedom@ubuntu.com>
2024-07-08 19:24:43 +0100

    Add more wayland checks, enable building quicknote under wayland

--- budgie-app-launcher/src/budgie-app-launcher/AppLauncherApplet.py
+++ budgie-app-launcher/src/budgie-app-launcher/AppLauncherApplet.py
@@ -14,13 +14,17 @@ import gi.repository
 
 gi.require_version('Gtk', '3.0')
 gi.require_version('GMenu', '3.0')
-gi.require_version('Budgie', '1.0')
 gi.require_version('Libxfce4windowing', '0.0')
+if Libxfce4windowing.windowing_get() == \
+                Libxfce4windowing.Windowing.WAYLAND:
+    gi.require_version('Budgie', '2.0')
+else:
+    gi.require_version('Budgie', '1.0')
+from gi.repository import Libxfce4windowing
 from gi.repository import Budgie
 from gi.repository import GMenu
 from gi.repository import Gtk
 from gi.repository import Gdk
-from gi.repository import Libxfce4windowing
 from AppButton import AppButton
 from PanelButton import PanelButton
 from ArrowButton import ArrowButton
