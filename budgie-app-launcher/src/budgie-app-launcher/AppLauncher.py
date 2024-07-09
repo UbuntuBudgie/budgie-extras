a008d6c9ed59cce7be94261929294c9dc280d64d
David Mohammed <fossfreedom@ubuntu.com>
2024-07-09 21:44:18 +0100

    Ensure we check for wayland correctly for AppLauncher before loading budgie gir

--- budgie-app-launcher/src/budgie-app-launcher/AppLauncher.py
+++ budgie-app-launcher/src/budgie-app-launcher/AppLauncher.py
@@ -12,7 +12,12 @@
 
 import gi.repository
 
-gi.require_version('Budgie', '1.0')
+gi.require_version('Libxfce4windowing', '0.0')
+from gi.repository import Libxfce4windowing
+if Libxfce4windowing.windowing_get() == Libxfce4windowing.Windowing.WAYLAND:
+    gi.require_version('Budgie', '2.0')
+else:
+    gi.require_version('Budgie', '1.0')
 from gi.repository import Budgie
 from gi.repository import GObject
 from AppLauncherApplet import AppLauncherApplet
