f51b06a0ebe7512d790113b5abf3bf7c4701f942
David Mohammed <fossfreedom@ubuntu.com>
2026-04-05 15:18:40 +0100

    v2.2.3 release

--- mkrelease.sh
+++ mkrelease.sh
@@ -4,7 +4,7 @@ set -e
 git submodule init
 git submodule update
 
-VERSION="2.2.2"
+VERSION="2.2.3"
 NAME="budgie-extras"
 git-archive-all.sh --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
 xz -9 "${NAME}-${VERSION}.tar"
